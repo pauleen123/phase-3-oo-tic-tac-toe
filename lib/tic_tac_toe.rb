030188efe6a9e61f195a7056e9f3e585e14c12cb
+ <+>
2022-10-19 10:34:10 +0300

    commit tree 55d7e12c1b8196c93250dc726c81a965b53a9853

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,25 @@
+class TicTacToe
+
+   def initialize(board=[" "," "," "," "," "," "," "," "," "])
+    @board = board
+   end
+
+   WIN_COMBINATIONS = 
+    [
+        [0, 1, 2], 
+        [3, 4, 5], 
+        [6, 7, 8], 
+        [0, 3, 6], 
+        [1, 4, 7], 
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [6, 4, 2]
+    ]
+
+    def display_board
+       puts board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+        
+    end 
+   
+ 
+end
